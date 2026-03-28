91874fc095151b1c943818eb2bcf8b9415dc9b3f
Rawiyah <you@example.com>
2026-03-28 00:34:54 -0600

    Fix vite config and add icon

--- vite.config.js
+++ vite.config.js
@@ -16,14 +16,18 @@ export default defineConfig({
         display: 'standalone',
         start_url: '/',
         icons: [
-  {
-    src: '/icon-512.png',
-    sizes: '192x192',
-    type: 'image/png',
-  },
-  {
-    src: '/icon-512.png',
-    sizes: '512x512',
-    type: 'image/png',
-  },
-],
+          {
+            src: '/icon-512.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+})

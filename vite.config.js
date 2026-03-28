3d1c87d136c633f4066ee269d553648890424d67
Rawiyah <you@example.com>
2026-03-28 00:25:31 -0600

    Add app icon

--- vite.config.js
+++ vite.config.js
@@ -16,18 +16,14 @@ export default defineConfig({
         display: 'standalone',
         start_url: '/',
         icons: [
-          {
-            src: '/icons.svg',
-            sizes: '192x192',
-            type: 'image/svg+xml',
-          },
-          {
-            src: '/icons.svg',
-            sizes: '512x512',
-            type: 'image/svg+xml',
-          },
-        ],
-      },
-    }),
-  ],
-})
+  {
+    src: '/icon-512.png',
+    sizes: '192x192',
+    type: 'image/png',
+  },
+  {
+    src: '/icon-512.png',
+    sizes: '512x512',
+    type: 'image/png',
+  },
+],
